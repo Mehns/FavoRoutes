4a2d90bf85357f495e5aadff43a3a5db94d6cbba
Christian Mehns <s55689@beuth-hochschule.de>
2016-03-08 12:14:41 +0100

    pwd change

--- Controller.php
+++ Controller.php
@@ -3,7 +3,7 @@ function __autoload($className){
     include_once("models/$className.php");
 }
 
-$users=new User("localhost","root","iH810iS@N8","locations");
+$users=new User("localhost","root","password","locations");
 
 if(!isset($_POST['action'])) {
     print json_encode(0);
